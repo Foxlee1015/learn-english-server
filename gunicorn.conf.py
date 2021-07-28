532bcad55fa40b255f6b976e8e691f33f93665a4
Daehan <dh16931@gmail.com>
2021-07-28 18:14:41 +0900

    chore: update port

--- gunicorn.conf.py
+++ gunicorn.conf.py
@@ -1,5 +1,5 @@
 # https://docs.gunicorn.org/en/stable/settings.html
-bind = '0.0.0.0:8000'
+bind = '0.0.0.0:8001'
 worker_class = 'sync' # default = sync
 workers = 2 # default = 1
 loglevel = 'debug'
