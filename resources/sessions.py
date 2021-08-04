350671c5d01762582e49a40b7dce75e8a34f66c3
Daehan <dh16931@gmail.com>
2021-08-04 11:43:37 +0900

    feat: return user info after validating session

--- resources/sessions.py
+++ resources/sessions.py
@@ -59,8 +59,13 @@ class SessionVlidation(CustomResource):
     def get(self, **kwargs):
         '''Check if session is valid'''
         try:
-            status = 200 if kwargs["user_info"] is not None else 401
-            return self.send(status=status)
+            user={}
+            status = 401
+            if kwargs["user_info"] is not None:
+                status = 200
+                user["name"] = kwargs["user_info"]["name"]
+                user["is_admin"] = 1 if kwargs["user_info"]["user_type"] == 0 else 0
+            return self.send(status=status, result=user)
         except:
             traceback.print_exc()
             return self.send(status=500)
