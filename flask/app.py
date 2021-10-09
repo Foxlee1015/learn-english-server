8c32687410d365ff7e5b7a8e4c0615af246e5b84
daehan <dh16931@gmail.com>
2021-10-10 08:11:16 +0900

    feat: update verbs in cache job

--- flask/app.py
+++ flask/app.py
@@ -7,6 +7,8 @@ from resources import blueprint as api
 from core.db import init_db
 from core.mongo_db import mongo_uri, mongo
 from core.errors import DbConnectError
+from resources.verbs import update_unique_verbs_job
+
 
 def init_settings():
     try:
@@ -17,35 +19,36 @@ def init_settings():
     except:
         traceback.print_exc()
 
+
 # thread
 def background_task():
-    while True:
-        print("background_task")
-        # backup_db()
+    update_unique_verbs_job()
+
 
 def set_mongodb_indexes():
     print("set mongodb indexes")
-    mongo.db.idioms.create_index([("$**","text")])
-    mongo.db.user_like_idiom.create_index([("userId",1)])
-    mongo.db.user_like_idiom.create_index([("idiomId",1)])
-    mongo.db.phrasal_verbs.create_index([("$**","text")])
-    mongo.db.user_like_phrasal_verb.create_index([("userId",1)])
-    mongo.db.user_like_phrasal_verb.create_index([("phrasalVerbId",1)])
+    mongo.db.idioms.create_index([("$**", "text")])
+    mongo.db.user_like_idiom.create_index([("userId", 1)])
+    mongo.db.user_like_idiom.create_index([("idiomId", 1)])
+    mongo.db.phrasal_verbs.create_index([("$**", "text")])
+    mongo.db.user_like_phrasal_verb.create_index([("userId", 1)])
+    mongo.db.user_like_phrasal_verb.create_index([("phrasalVerbId", 1)])
+
 
 def create_app():
     app = Flask(__name__)
-    app.config['SECRET_KEY'] = "ssseetrr"
+    app.config["SECRET_KEY"] = "ssseetrr"
     app.config["MONGO_URI"] = mongo_uri
     mongo.init_app(app)
-        
+
     CORS(app, resources={r"/api/*": {"origins": "*"}})
 
-    app.register_blueprint(api, url_prefix='/api')
-    # api.init_app(app) 
+    app.register_blueprint(api, url_prefix="/api")
+    # api.init_app(app)
     # https://flask-restplus.readthedocs.io/en/stable/scaling.html
     # Calling Api.init_app() is not required here because registering the blueprint with the app takes care of setting up the routing for the application.
     init_settings()
 
-    # background_task()
+    background_task()
 
     return app
--- flask/resources/verbs.py
+++ flask/resources/verbs.py
@@ -1,22 +1,32 @@
 import traceback
+import time
 from flask_restplus import Namespace, reqparse
+from threading import Thread
 
-
-from core.db import get_verbs, insert_verbs, delete_verbs
+from core.variables import UPDATE_VERB_LIST_TIME
+from resources.phrasal_verbs import get_all_unique_field_values
 
 from core.resource import (
     CustomResource,
 )
-from core.utils import token_required
-from resources.phrasal_verbs import get_all_unique_field_values, sync_phrasal_verbs
 
 api = Namespace("verbs", description="Verbs related operations")
 
-parser_create = reqparse.RequestParser()
-parser_create.add_argument("name", type=str, required=True, help="Unique verb")
 
-parser_header = reqparse.RequestParser()
-parser_header.add_argument("Authorization", type=str, required=True, location="headers")
+unique_verbs = []
+
+
+def update_unique_verbs():
+    while True:
+        global unique_verbs
+        unique_verbs = get_all_unique_field_values("verb")
+        time.sleep(UPDATE_VERB_LIST_TIME)
+
+
+def update_unique_verbs_job():
+    thread = Thread(target=update_unique_verbs)
+    thread.daemon = True
+    thread.start()
 
 
 @api.route("/")
@@ -24,73 +34,7 @@ class Verbs(CustomResource):
     @api.doc("list_verbs")
     def get(self):
         try:
-            return self.send(status=200, result=get_verbs())
-        except:
-            traceback.print_exc()
-            return self.send(status=500)
-
-    @api.expect(parser_create, parser_header)
-    @token_required
-    def post(self, **kwargs):
-        try:
-            if not self.is_admin(kwargs["user_info"]):
-                return self.send(status=403)
-
-            args = parser_create.parse_args()
-            name = args["name"]
-            if get_verbs(name=name):
-                return self.send(status=200)
-            result = insert_verbs(names=[name])
-            if result:
-                # sync_phrasal_verbs()
-                return self.send(status=201)
-            else:
-                return self.send(status=400)
-        except:
-            traceback.print_exc()
-            return self.send(status=500)
-
-
-@api.route("/<int:id_>")
-@api.param("id", "The verb identifier")
-class verb(CustomResource):
-    @api.doc("get_verb")
-    def get(self, id_):
-        verb = get_verbs(id_=id_)
-        if verb is None:
-            return self.send(status=200, result=None)
-        return self.send(status=200, result=verb)
-
-    @api.doc("delete_verb")
-    @api.expect(parser_header)
-    @token_required
-    def delete(self, id_, **kwargs):
-        try:
-            if not self.is_admin(kwargs["user_info"]):
-                return self.send(status=403)
-            result = delete_verbs(ids=[id_])
-            if result:
-                return self.send(status=200)
-            return self.send(status=400, message="Check verb id")
-
-        except:
-            traceback.print_exc()
-            return self.send(status=500)
-
-
-@api.route("/async")
-class AsyncVerbs(CustomResource):
-    @api.doc("async_verb")
-    @api.expect(parser_header)
-    @token_required
-    def put(self, **kwargs):
-        try:
-            if not self.is_admin(kwargs["user_info"]):
-                return self.send(status=403)
-            verbs = get_all_unique_field_values("verb")
-            insert_verbs(names=verbs)
-            return self.send(status=200)
-
+            return self.send(status=200, result=unique_verbs)
         except:
             traceback.print_exc()
             return self.send(status=500)
